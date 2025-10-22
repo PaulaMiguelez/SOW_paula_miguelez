fbbd5524b458ed51d07c1d1402308aae5a2fde0f
Paula M <paula.miguelez@alumnos.udit.es>
2025-10-22 03:19:06 +0200

    cambios2

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+
+//     // MENÚ RESPONSIVE
+//     const btn_menu = document.querySelector(".burger i");
+
+//     btn_menu.addEventListener("click", function(){ 
+//         const menu = document.querySelector(".menu");
+//         menu.classList.toggle("menu-show");
+//         btn_menu.classList.toggle("fa-bars");
+//         btn_menu.classList.toggle("fa-times");
+    
+//     });
+// });
